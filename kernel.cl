349a9f070a4fcf475da72b87393f41aac75f4825
maisumhaider <maisum.haider@gmail.com>
2018-10-16 10:50:21 +0200

    WIP Adding Matmul Dataflow

--- kernel.cl
+++ kernel.cl
@@ -2,7 +2,33 @@ __kernel void vadd(__global const int* A, __global const int* B, __global int* C
   int id = get_global_id(0);
   printf("Executing Kernel %d\n",id);
   int i;
+  int index = id*block_size[0];
   for(i=0;i<block_size[0];i++){
-    C[id+i] = A[id+i]+B[id+i];
+    C[index+i] = A[index+i]+B[index+i];
+  }
+}
+
+__kernel void mmult(__global const float* matA,  //Read-only input matrix1
+                    __global const float* matB,  //Read-only input matrix2
+                    __global float* matC,  //Output matrix
+                    __global const int* dimM,
+                    __global const int* dimK,
+                    __global const int* dimN,
+                    __global const int* block_size){
+  int id = get_global_id(0);//id/dimM;
+  int k,i,startRow,startCol;
+  int index,index2,resIndex;
+  float acc = 0.0f;
+  startRow = (id * block_size[0])/dimM[0];
+  startCol = (id * block_size[0])%dimM[0];
+  printf("Executing Kernel\n");
+  for(i=0;i<block_size[0];i++) {
+    for ( k = 0 ; k < dimK[0] ; k++ ) {
+      index = startRow * dimK[0] + k;
+      index2 = startCol* dimK[0] + k;
+      acc += matA[ index ] * matB[ index2 ];
+    }
+    resIndex = startRow * dimN[0] + startCol;
+    matC[ resIndex ] = acc;
   }
 }
--- main.cpp
+++ main.cpp
@@ -2,9 +2,33 @@
 #include <CL/cl2.hpp>
 #include <fstream>
 #include <cassert>
+#include <cmath>
 #define LOAD 4096
+#define CORES 4
 using std::cout;
 using std::endl;
+
+template <typename T>
+struct aligned_allocator {
+  using value_type = T;
+  T *allocate (std::size_t num) {
+    void *ptr = nullptr;
+    if ( posix_memalign (&ptr , 4096 , num * sizeof (T))) {
+      throw std::bad_alloc ();
+    }
+    return reinterpret_cast<T *>(ptr);
+  }
+  void deallocate (T *p , std::size_t num) {
+    free (p);
+  }
+};
+bool cmpFloat (float A , float B , float tolerance = 0.0f);
+float RandomFloat (float min = 0.0 , float max = 1.0);
+
+void mmult(const int id, const float* matA, const float* matB,
+           float* matC, const int* dimM, const int* dimK, const int* dimN, const int* block_size);
+
+void mmult_OpenCL_coalesced(cl::CommandQueue& q,cl::Context &context,cl::Kernel &kernel);
 
 int main () {
   cl_int err;
@@ -35,8 +59,9 @@ int main () {
   std::vector<int> B = std::vector<int>(LOAD);  
   std::vector<int> C = std::vector<int>(LOAD,0);
   std::vector<int> C_sim = std::vector<int>(LOAD,0);
-  int block_size;
-  
+  int global = 4;
+  int block_size=LOAD/global;
+
   for(auto &x:B)x = rand()%20;
   for(int i =0;i<LOAD;i++){
     C_sim[i]=A[i]+B[i];
@@ -58,13 +83,56 @@ int main () {
   queue.enqueueWriteBuffer (buffer_block,CL_TRUE,0, sizeof (int),&block_size);
 
   cl::Event event;
-  queue.enqueueNDRangeKernel (vadd, 0, cl::NDRange(4), cl::NullRange, NULL, &event);
+  queue.enqueueNDRangeKernel (vadd, 0, cl::NDRange(global), cl::NullRange, NULL, &event);
   event.wait ();
 
   queue.enqueueReadBuffer (buffer_C,CL_TRUE,0, sizeof (int)*LOAD,C.data ());
   queue.finish ();
   bool match = true;
-
+  for(int i =0;i<LOAD;i++){
+    if (C_sim[i]!=C[i]){
+      cout<<"Id:"<<i<<" SW Res:"<<C_sim[i]<<" HW Res:"<<C[i]<<endl;
+      match= false;
+      break;
+    }
+  }
 
   return match?EXIT_SUCCESS:EXIT_FAILURE;
+}
+void mmult (const int id ,
+            const float *matA ,
+            const float *matB ,
+            float *matC ,
+            const int *dimM ,
+            const int *dimK ,
+            const int *dimN ,
+            const int *block_size) {
+  int k,i,startRow,startCol;
+  int index,index2,resIndex;
+  float acc = 0.0f;
+  startRow = (id * block_size[0])/dimM[0];
+  startCol = (id * block_size[0])%dimM[0];
+  printf("Executing Kernel\n");
+  for(i=0;i<block_size[0];i++) {
+    for ( k = 0 ; k < dimK[0] ; k++ ) {
+      index = startRow * dimK[0] + k;
+      index2 = startCol* dimK[0] + k;
+      acc += matA[ index ] * matB[ index2 ];
+    }
+    resIndex = startRow * dimN[0] + startCol;
+    matC[ resIndex ] = acc;
+  }
+}
+bool cmpFloat (float A , float B , float tolerance) {
+  if ( fabs (A - B) > std::numeric_limits<float>::epsilon ()) {
+    cout << "Mismatch " << fabs (A - B) << endl;
+    return false;
+  }
+  return true;
+}
+float RandomFloat (float min , float max) {
+  assert(max > min);
+  float random = (( float ) rand ()) / ( float ) RAND_MAX;
+  float range = max - min;
+  return ( random * range ) + min;
 }
